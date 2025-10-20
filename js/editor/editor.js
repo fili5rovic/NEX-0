2b3f4830991b76c29f762d581d248f85b65ff047
fili5rovic <fili5rovic@gmail.com>
2025-10-20 05:19:44 +0200

    Refactor editor: improve overflow handling in CSS and enhance editor initialization in JavaScript

--- js/editor/editor.js
+++ js/editor/editor.js
@@ -1,35 +1,47 @@
-const editor = document.getElementById('editor');
-const highlightedCode = document.getElementById('highlighted-code');
+export class EditorHandler {
+     constructor(cpu, editor, highlightedCode, highlightLayer) {
+        this.cpu = cpu;
+        this.editor = editor;
+        this.highlightedCode = highlightedCode;
+        this.highlightLayer = highlightLayer
 
-function highlight(text) {
-    text = text
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
+        this.editor.addEventListener('input', this.updateHighlight);
+        this.editor.addEventListener('scroll', this.handleScroll);
 
-    const commentRegex = /(#.*$)/gm;
-    text = text.replace(commentRegex, '<span class="comment">$1</span>');
-    const keywordRegex = /\b(add|sub|mul|div|load|store|neg|nop|halt|jmp|jz|jnz|jg|jge|jl|jle)\b/gi;
-    text = text.replace(keywordRegex, '<span class="keyword">$1</span>');
-    const registerRegex = /\b([Rr][0-9]+)\b/g;
-    text = text.replace(registerRegex, '<span class="register">$1</span>');
-    const numberRegex = /(#?\b\d+\b|0x[0-9A-Fa-f]+)/g;
-    text = text.replace(numberRegex, '<span class="number">$1</span>');
+        this.updateHighlight();
+    }
 
-    return text;
+    handleScroll = () => {
+        this.highlightLayer.scrollTop = this.editor.scrollTop;
+        this.highlightLayer.scrollLeft = this.editor.scrollLeft;
+    }
+
+    updateHighlight = () => {
+        const text = this.editor.value;
+        this.highlightedCode.innerHTML = this.highlight(text) + '\n';
+    }
+
+    highlight(text) {
+        text = text
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;");
+
+        const commentRegex = /(#.*$)/gm;
+        text = text.replace(commentRegex, '<span class="comment">$1</span>');
+        const keywordRegex = /\b(add|sub|mul|div|load|store|neg|nop|halt|jmp|jz|jnz|jg|jge|jl|jle)\b/gi;
+        text = text.replace(keywordRegex, '<span class="keyword">$1</span>');
+        const registerRegex = /\b([Rr][0-9]+)\b/g;
+        text = text.replace(registerRegex, '<span class="register">$1</span>');
+        const numberRegex = /(#?\b\d+\b|0x[0-9A-Fa-f]+)/g;
+        text = text.replace(numberRegex, '<span class="number">$1</span>');
+
+        return text;
+    }
+
+    updateHighlight() {
+        const text = this.editor.value;
+        this.highlightedCode.innerHTML = this.highlight(text) + '\n';
+    }
 }
 
-function updateHighlight() {
-    const text = editor.value;
-    highlightedCode.innerHTML = highlight(text) + '\n';
-}
-
-editor.addEventListener('scroll', () => {
-    const highlightLayer = document.getElementById('highlight-layer');
-    highlightLayer.scrollTop = editor.scrollTop;
-    highlightLayer.scrollLeft = editor.scrollLeft;
-});
-
-editor.addEventListener('input', updateHighlight);
-
-updateHighlight();
--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ import CPU from '../js/simulator/cpu.js';
 import { extractCode, getLabelsMap } from './simulator/parser.js';
 import '../js/editor/editor.js'
 import {OneAddrArchitecture} from './architecture/architecture.js'
+import { EditorHandler } from '../js/editor/editor.js';
 let cpu = new CPU(new OneAddrArchitecture());
 
 const runBtn = document.querySelector('#runBtn');
-const editor = document.querySelector('#editor');
+const editor = document.getElementById('editor');
+const highlightedCode = document.getElementById('highlighted-code');
+const highlightedLayer = document.getElementById('highlight-layer');
+
+let editorHandler = new EditorHandler(cpu, editor, highlightedCode, highlightedLayer);
 
 runBtn.addEventListener('click',()=>{
     let code = extractCode(editor.value);
