a6cba86594c26f42c6d0cbb9f6e9a0ee5f4a595f
fili5rovic <fili5rovic@gmail.com>
2025-11-15 03:57:15 +0100

    - changed project structure - everything is now loaded from levels that are written in JSON, it's much easier than writing html. This is done for future custom level support

---
+++ js/core/cpu/editor/programs/programs.js
@@ -0,0 +1,20 @@
+export function getProgram(name) {
+    return PROGRAMS[name]?.code;
+}
+
+export function addInitialPrograms(initialPrograms) {
+    for(const program of initialPrograms) {
+        addProgram(program.name, program.code);
+    }
+}
+
+function addProgram(name, code) {
+    PROGRAMS[name] = { code: code };
+}
+
+
+const PROGRAMS = {
+    'test' : {
+        code : `LOAD #5\nLOOP: DEC\nJNZ LOOP`
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import {system} from "./system.js";
+import {loadLevel} from "./level/levelLoader.js";
 
+await loadLevel('testLevel')
 system.generateHTML();
